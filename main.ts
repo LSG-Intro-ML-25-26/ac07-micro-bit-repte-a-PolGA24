06e95e08cc071a1c12c0f996d33e098608be68be
Pol González Arribas <polgonzarri@gmail.com>
2025-12-02 14:50:58 +0100

    pedra paper tisora basic

--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
+let hand = 0
+let num_random = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(0, 3)
+    if (hand == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (hand == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    num_random += randint(1, 3)
+    basic.showString("Has escogido!")
+    basic.showLeds(`
+        . # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # .
+        `)
+    if (num_random == 1) {
+        basic.showString("PERDI :(, escogi:")
+        basic.showIcon(IconNames.Target)
+    } else if (num_random == 2) {
+        basic.showString("GANEE :), escogi:")
+        basic.showIcon(IconNames.Scissors)
+    } else if (num_random == 3) {
+        basic.showString("EMPATE, escogi:")
+        basic.showLeds(`
+            . # . # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # .
+            `)
+    }
+})
 basic.forever(function () {
 	
 })
